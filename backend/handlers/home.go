a8bec21719d32860a22263a08a83a0aca2885929
Yossf-jaouhar <jwhryosf@gmail.com>
2025-03-08 01:42:19 +0000

    fixing post style

--- backend/handlers/home.go
+++ backend/handlers/home.go
@@ -2,18 +2,12 @@ package handlers
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	if r.Method == "post" {
-		fmt.Println("here")
 		SignInHandler(w, r, db)
 	}
-	// if r.Method != http.MethodGet {
-	// 	errors.SendError("method not allowed", http.StatusMethodNotAllowed, w)
-	// 	return
-	// }
 	http.ServeFile(w, r, "./frontend/templete/index.html")
 }
