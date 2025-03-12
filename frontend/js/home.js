3601b9587fa83abf2f592148ac7d96d72f00ba9e
Yossf-jaouhar <jwhryosf@gmail.com>
2025-03-12 00:01:51 +0000

    working in addpost

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -96,7 +96,6 @@ const creatpost = `
 </div>
 `
 function statement() {
-    console.log("hi")
     const addpost = document.getElementById("AddPost")
     addpost.addEventListener("click",  function () {
         addpost.outerHTML = creatpost
@@ -114,19 +113,49 @@ function creat() {
             const content = document.getElementById("contentID")
             const titlevalue = title.value.trim()
             const contentvalue = content.value.trim()
-            if (titlevalue.length > 5) {
+            if (titlevalue.length > 50) {
                 title.style.borderColor = "red";
                 errortitle.style.display = "block";
 
                 errortitle.textContent = "your title it most have 50 char"
                 return 
             }
-            if (contentvalue.length > 2) {
+            if (contentvalue.length > 200) {
                 content.style.border = "red"
                 errorcontent.style.display = "block"
                 errorcontent.textContent = "your content it most have 200 char"
                 return
             }
-        })
+            const postdata = {
+                title: titlevalue,
+                content: contentvalue
+            }
+
+            try {
+                const response = await fetch("http://localhost:8080/addpost" , {
+                    method : "POST",
+                    headers : {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(postdata),
+                })
+
+                if (response.ok) {
+                    throw new Error(`HTTP Error: ${response.status}`)
+                }
+
+                const data = await response.json()
+                if (data.success) {
+                    window.location.href = "http://localhost:8080/";
+                    return
+                } else {
+                    alert("it is failed");
+                }
+            } catch (error) {
+                console.error("Error:", error);
+                alert("An error occurred. Please try again later.");
+            }
+            
+        });
     }
 }
